d2bb94090fc473764f0ca654b58f3face8ed95a5
cuiltyv <a01721636@itesm.mx>
2024-04-25 14:06:03 -0600

    Pruebas de login y registro

--- cypress/cypress/e2e/login.cy.js
+++ cypress/cypress/e2e/login.cy.js
@@ -7,7 +7,7 @@ describe("Probar la funcionalidad de login", () => {
     cy.wait(1000);
     cy.get("#username").type("A555");
     cy.get("#password").type("Cuilty1!");
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 
@@ -15,7 +15,7 @@ describe("Probar la funcionalidad de login", () => {
     cy.wait(1000);
     cy.get("#username").type("A444");
     cy.get("#password").type("FakePassword123!");
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 
@@ -23,27 +23,27 @@ describe("Probar la funcionalidad de login", () => {
     cy.wait(1000);
     cy.get("#username").type("2351512253");
     cy.get("#password").type("password123!");
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 
   it("Probar sin password", () => {
     cy.wait(1000);
     cy.get("#username").type("A555");
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 
   it("Probar sin username", () => {
     cy.wait(1000);
     cy.get("#password").type("password123!");
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 
   it("Probar sin username ni password", () => {
     cy.wait(1000);
-    cy.get(".my-3").click();
+    cy.get(".flex.items-center > .inline-flex").click();
     cy.wait(4000);
   });
 });
